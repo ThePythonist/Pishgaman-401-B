11d25aa0356112fb547d5ea7800a5184147d146d
Arash <53490252+ThePythonist@users.noreply.github.com>
2022-08-22 18:38:36 +0430

    Add files via upload

---
+++ 24.py
@@ -0,0 +1,10 @@
+# i = 1
+
+# while i < 20 :
+#     if i % 2 == 0 :
+#         print(i)
+#     i += 1
+
+for i in range(1,20):
+    if i % 2 == 0 :
+        print(i)
---
+++ 25.py
@@ -0,0 +1,14 @@
+i = 1
+
+# while i <= 20 :
+#     if i % 2 != 0 :
+#         print(i)
+#     i += 1
+
+# while i <= 20 :
+#     print(i)
+#     i += 2
+
+for i in range(1,20):
+    if i % 2 != 0 :
+        print(i)
